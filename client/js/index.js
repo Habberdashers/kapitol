bbf2d284304752078f586381528c8974ddda294d
Megan Smith <meg.eli.smi@gmail.com>
2017-01-21 00:32:04 -0800

    react router added

--- client/js/index.js
+++ client/js/index.js
@@ -2,17 +2,21 @@ import 'babel-polyfill';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PageContainer from './components/pageContainer'
+import PageContainer from './components/pageContainer';
+import SearchForm from './components/searchForm';
 import store from './store';
 import {Provider} from 'react-redux';
+import { Router, Route, hashHistory } from 'react-router';
 
 
 document.addEventListener('DOMContentLoaded', () => {
 	ReactDOM.render(
 		<Provider store={store}>
-			<PageContainer />
+			<Router history={hashHistory}>
+				<Route path="/" component={PageContainer} />
+				<Route path="/test" component={SearchForm} />
+			</Router>
 		</Provider>,
 	document.getElementById('app'))});
