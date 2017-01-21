dd8cb0e4cb4daff815361190009614b0059c7a6b
Megan Smith <meg.eli.smi@gmail.com>
2017-01-21 02:20:15 -0800

    landing page routing for real to about

---
+++ client/js/components/memberPage.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+
+class memberPage extends React.Component {
+	constructor (props) {
+		super(props); 
+	}
+
+	render () {
+		return (
+			
+		)
+	}
+}
+
+export default 
