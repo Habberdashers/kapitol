c386c0f5e91febde4146f58944f93fe62524d53a
Sergio Gonzale <sergio.gnzlz.25@gmail.com>
2017-01-21 13:30:52 -0800

    Members page formating and search form was made to run independently

--- client/js/components/searchForm.js
+++ client/js/components/searchForm.js
@@ -1,23 +1,25 @@
-import React from 'react'; 
+import React from 'react';
+import {hashHistory} from 'react-router';
 
 const SearchForm = (props) => {
-	let searchTerm; 
+	let searchTerm;
 
 	function grabInput (event) {
 		event.preventDefault();
 		console.log(searchTerm.value);
-		props.onSubmit(searchTerm.value);
+		hashHistory.push('/member');
+
 	}
 
 	return (
 		<div className="search-container">
-			<form className="search-form" onSubmit={grabInput}>
+			<form className="search-form"  onSubmit={grabInput}>
 	  			<input className="user-input-box" type="text" placeholder="" ref={element => searchTerm = element} />
 			</form>
 		</div>
 	)
 }
 
-export default SearchForm; 
+export default SearchForm;
 
 //<input className="submit-button" type="submit" value="Submit" />
